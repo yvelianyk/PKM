4e10e7380c62f5a042edc0e0f66a85a679ef25fa
Yaroslav Velianyk <yvelianyk@softjourn.com>
2016-11-16 10:09:52 +0200

    added new future2

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-/* Added new future*/
+/* Added new future */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
