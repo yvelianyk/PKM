7ec425552906dea506678d9ba52edad0d2c4efde
Yaroslav Velianyk <yvelianyk@softjourn.com>
2016-11-16 10:04:58 +0200

    added some comments

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
+// comment
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
