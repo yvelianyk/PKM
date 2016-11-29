ad99194b1b39dee82c86ebeb45cd7cacbd596fe6
Yaroslav Velianyk <yvelianyk@softjourn.com>
2016-11-29 23:17:43 +0200

    added some images and text templates

---
+++ app/admin/admin.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'admin-component',
+    templateUrl: './app/admin/admin.component.html',
+})
+
+export class AdminComponent {}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule }   from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
@@ -9,10 +12,26 @@ import { FooterComponent } from './footer/footer.component';
 @NgModule({
     imports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        RouterModule.forRoot([
+            {
+                path: 'admin',
+                component: AdminComponent
+            },
+            {
+                path: 'home',
+                component: ContentComponent
+            },
+            {
+                path: '',
+                redirectTo: '/home',
+                pathMatch: 'full'
+            }
+        ])
     ],
     declarations: [
         AppComponent,
+        AdminComponent,
         NavBarComponent,
         ContentComponent,
         FooterComponent
